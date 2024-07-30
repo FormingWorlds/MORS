41f5172237a933078445f33b574ffc2b91920be0
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2024-07-30 12:25:22 +0200

    Add download data cli.

---
+++ src/mors/cli.py
@@ -0,0 +1,29 @@
+import click
+
+@click.group()
+def cli():
+    pass
+
+@click.group()
+def download():
+    """Download data"""
+    pass
+
+@click.command()
+def spada():
+    """Download Spada evolution tracks."""
+    from .data import DownloadEvolutionTracks
+    DownloadEvolutionTracks("/Spada")
+
+@click.command()
+def baraffe():
+    """Download Baraffe evolution tracks."""
+    from .data import DownloadEvolutionTracks
+    DownloadEvolutionTracks("/Baraffe")
+
+cli.add_command(download)
+download.add_command(baraffe)
+download.add_command(spada)
+
+if __name__ == '__main__':
+    cli()
