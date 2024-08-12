5697573945c0c7e0a8bc08d1cc9468be2176051b
Stef Smeets <s.smeets@esciencecenter.nl>
2024-08-12 14:32:00 +0200

    Use default data directory

--- src/mors/cli.py
+++ src/mors/cli.py
@@ -27,10 +27,18 @@ def all():
     from .data import DownloadEvolutionTracks
     DownloadEvolutionTracks()
 
+@click.command()
+def env():
+    """Show environment variables and locations"""
+    from mors.data import FWL_DATA_DIR
+
+    click.echo(f'FWL_DATA location: {FWL_DATA_DIR}')
+
 cli.add_command(download)
 download.add_command(spada)
 download.add_command(baraffe)
 download.add_command(all)
+cli.add_command(env)
 
 if __name__ == '__main__':
     cli()
