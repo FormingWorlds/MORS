05c22595f913e6303871376b5ca495830bd2ea16
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2024-08-20 14:40:32 +0200

    Adding Spada test and cleaning Baraffe test.

--- tests/test_baraffe.py
+++ tests/test_baraffe.py
@@ -3,13 +3,14 @@ import pytest
 from numpy.testing import assert_allclose
 
 TEST_DATA = (
-    ((0.047, 8.5e7, 0.75),(0.0007430607161602045, 0.143, 1.7980958798328914)),
-    ((1.113, 3.2e9, 1.05),(1.6544100501920276, 1.1821623140636999, 2042.5638079262828))
+    ((0.047, 8.5e7, 0.75),(0.74306071e-3, 0.14300000, 1.79809587  )),
+    ((1.113, 3.2e9, 1.05),(1.65441005   , 1.18216231, 2.04256380e3)),
 )
 
 @pytest.mark.parametrize("inp,expected", TEST_DATA)
 def test_baraffe(inp, expected):
 
+    mors.DownloadEvolutionTracks('Baraffe')
     baraffe = mors.BaraffeTrack(inp[0])
     ret = (
          baraffe.BaraffeLuminosity(inp[1]),
