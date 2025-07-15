281989ef0d44938e843c5d29a8f437185708539f
Laurent Soucasse <135331272+lsoucasse@users.noreply.github.com>
2025-07-15 10:08:42 +0200

    Add Zenodo download option.

--- src/mors/data.py
+++ src/mors/data.py
@@ -2,6 +2,7 @@ import logging
 import os
 import subprocess
 from pathlib import Path
+from time import sleep
 
 import platformdirs
 from osfclient.api import OSF
@@ -16,7 +17,47 @@ log.info(f'FWL data location: {FWL_DATA_DIR}')
 #project ID of the stellar evolution tracks folder in the OSF
 project_id = '9u3fb'
 
-def download_folder(*, storage, folders: list[str], data_dir: Path):
+def get_zenodo_record(folder: str) -> str | None:
+    """
+    Get Zenodo record ID for a given folder.
+
+    Inputs :
+        - folder : str
+            Folder name to get the Zenodo record ID for
+
+    Returns :
+        - str | None : Zenodo record ID or None if not found
+    """
+    zenodo_map = {
+        'Baraffe': '15729114',
+        'Spada': '15729101',
+    }
+    return zenodo_map.get(folder, None)
+
+def download_zenodo_folder(folder: str, data_dir: Path):
+    """
+    Download a specific Zenodo record into specified folder
+
+    Inputs :
+        - folder : str
+            Folder name to download
+        - folder_dir : Path
+            local repository where data are saved
+    """
+
+    folder_dir = data_dir / folder
+    folder_dir.mkdir(parents=True)
+    zenodo_id = get_zenodo_record(folder)
+    cmd = [
+            "zenodo_get", zenodo_id,
+            "-o", folder_dir
+        ]
+    out = os.path.join(GetFWLData(), "zenodo.log")
+    log.debug("    logging to %s"%out)
+    with open(out,'w') as hdl:
+        subprocess.run(cmd, check=True, stdout=hdl, stderr=hdl)
+
+def download_OSF_folder(*, storage, folders: list[str], data_dir: Path):
     """
     Download a specific folder in the OSF repository
 
@@ -70,11 +111,38 @@ def DownloadEvolutionTracks(fname=""):
     else:
         raise ValueError(f"Unrecognised folder name: {fname}")
 
-    folders = [folder for folder in folder_list if not os.path.exists(os.path.join(data_dir, folder))]
+    for folder in folder_list:
+        folder_dir = data_dir / folder
+        max_tries = 2 # Maximum download attempts, could be a function argument
 
-    if folders:
-        log.info(f"Downloading MORS evolution tracks to {data_dir}")
-        download_folder(storage=storage, folders=folders, data_dir=data_dir)
+        if not folder_dir.exists():
+            log.info(f"Downloading stellar evolution tracks to {data_dir}")
+            for i in range(max_tries):
+                log.info(f"Attempt {i + 1} of {max_tries}")
+                success = False
+
+                try:
+                    download_zenodo_folder(folder = folder, data_dir=data_dir)
+                    success = True
+                except RuntimeError as e:
+                    log.error(f"Zenodo download failed: {e}")
+                    folder_dir.rmdir()
+
+                if not success:
+                    try:
+                        download_OSF_folder(storage=storage, folders=folder, data_dir=data_dir)
+                        success = True
+                    except RuntimeError as e:
+                        log.error(f"OSF download failed: {e}")
+
+                if success:
+                    break
+
+                if i < max_tries - 1:
+                    log.info("Retrying download...")
+                    sleep(5) # Wait 5 seconds before retrying
+                else:
+                    log.error("Max retries reached. Download failed.")
 
     if "Spada" in folders:
         #Unzip Spada evolution tracks
